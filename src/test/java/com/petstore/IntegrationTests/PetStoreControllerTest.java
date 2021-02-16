4e1011eff73befb6019a73ed3af8e60e534eb86f
Meet Joshi <meetjoshi93@gmail.com>
2021-02-16 15:05:21 -0600

    Added IT test for getAllAnimals

--- src/test/java/com/petstore/IntegrationTests/PetStoreControllerTest.java
+++ src/test/java/com/petstore/IntegrationTests/PetStoreControllerTest.java
@@ -1,15 +1,24 @@
 package com.petstore.IntegrationTests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.petstore.model.Animal;
+import com.petstore.repository.AnimalRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -20,6 +29,16 @@ public class PetStoreControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    AnimalRepository animalRepository;
+
+    ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper =  new ObjectMapper();
+    }
+
     @Test
     public void homePage() throws Exception {
         String response = mockMvc.perform(get("/home"))
@@ -27,6 +46,17 @@ public class PetStoreControllerTest {
                 .andReturn().getResponse().getContentAsString();
 
         assertEquals("Welcome to Pet Store", response);
+    }
 
+    @Test
+    public void getAllAnimals() throws Exception {
+        animalRepository.saveAll(List.of(
+                new Animal("101","Lion1","species",LocalDate.of(2015,12,27)
+                , "Male","Gold"),
+                new Animal("101","Lion2","species",LocalDate.of(2017,3,12)
+                        , "Female","Gold")));
+
+        mockMvc.perform(get("/animals"))
+                .andExpect(status().isOk());
     }
 }
